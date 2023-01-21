de5fbb77f9ff98a25bd91381adbd1573cf087814
Oleg <zerafperon@gmail.com>
2023-01-21 17:13:53 +0400

    add number3

--- number3/Program.cs
+++ number3/Program.cs
@@ -4,7 +4,7 @@
 //1 -> нет
 Console.WriteLine("Введите число дня недели: ");
 int a = Convert.ToInt32(Console.ReadLine());
-if (a > 1 && a < 6)
+if (a > 1 && a < 6  )
 {
     Console.WriteLine("Это рабочий день");
 }
